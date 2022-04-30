e64b24a0aea3c079f15f57497f862ac414aa49c2
kate <kateincoding@gmail.com>
2022-04-30 01:08:11 -0500

    [web_static]

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,6 +9,7 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.teardown_appcontext
 def teardown_db(exception):
