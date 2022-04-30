63ef16b0c472acd96f01eb7ea42a308ca78786dd
kate <kateincoding@gmail.com>
2022-04-30 00:12:24 -0500

    [readme +front]

--- web_dynamic/static/scripts/mvp.js
+++ web_dynamic/static/scripts/mvp.js
@@ -13,9 +13,16 @@ function main () {
 function dataUsers () {
   const url = `http://${HOST}:5001/api/v1/products/category/5`;
   console.log("fin");
-  $.get(url, function (data, txtStatus) {
+  $.get(url, function (data) {
     console.log("hola mundo");
     //$('.products').empty();
-    // console.log(data);
+    for (const product of data) {
+      $('section.products').append(`<article>
+      <div class="title_box">
+        <h2>${product.name}</h2>
+        <h3>holaaaaa</h3>
+      </div>
+      </article>`);
+    }
   });
 }
