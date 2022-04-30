5a00483a5d208abcb0a9bf504a944141f3087069
kate <kateincoding@gmail.com>
2022-04-30 02:21:54 -0500

    {web dynamic done}

--- web_dynamic/static/scripts/mvp.js
+++ web_dynamic/static/scripts/mvp.js
@@ -1,27 +1,30 @@
 $(document).ready(main);
 const HOST = '0.0.0.0';
 const checksCategories = {};
+let categoryId = "0";
 
 function main () {
   readCategories();
-  console.log("estoy aqui")
   dataUsers();
-  /* $(':button').click(function () {
+  $(':button').click(function () {
       dataUsers();
-  }); */
+  });
 }
 
 function dataUsers () {
-  const url = `http://${HOST}:5001/api/v1/products/category/5`;
+  const url = `http://${HOST}:5001/api/v1/products/category/${categoryId}`;
   console.log("fin");
   $.get(url, function (data) {
     console.log("hola mundo");
-    //$('.products').empty();
+    $('.products').empty();
     for (const product of data) {
       $('section.products').append(`<article>
       <div class="title_box">
         <h2>${product.name}</h2>
-        <h3>holaaaaa</h3>
+        <div class="price_by_night">$${product.price}</div>
+      </div>
+      <div class="information">
+        <img src="${product.url}" alt="${product.name}" width="100" height="70" >
       </div>
       </article>`);
     }
@@ -29,13 +32,15 @@ function dataUsers () {
 }
 
 function readCategories () {
-  $('.categories .popover').change(function () {
+  $('.categories .popover INPUT[type="checkbox"]').change(function () {
     if ($(this).is(':checked')) {
+      console.log("heheheheheh")
       checksCategories[$(this).attr('data-name')] = $(this).attr('data-id');
+      categoryId = $(this).attr('data-id');
     } else {
       delete checksCategories[$(this).attr('data-name')];
     }
-    const namesh4 = Object.keys(checksStates);
+    const namesh4 = Object.keys(checksCategories);
     $('.h4_categories').text(namesh4.sort().join(', '));
   });
 }
